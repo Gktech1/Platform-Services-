bdb6b41bab9bd8b27f6252f9cf90bae041a606cf
Gktech1 <favourblessing@gmail.com>
2022-08-03 16:46:00 +0100

    first commit

---
+++ Controllers/Api/PlatforrmsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformServices.Data;
+using PlatformServices.Dtos;
+using PlatformServices.Models;
+
+namespace PlatformServices.Controllers.Api
+{
+    [Route("api/[controller]")]
+    public class PlatformsController : Controller
+    {
+        private  readonly  IPlatformRepo _platformRepo;
+        private readonly  IMapper _mapper;
+        public PlatformsController(IPlatformRepo platformRepo, IMapper mapper)
+        {
+            _platformRepo = platformRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platform...");
+            var platformItem = _platformRepo.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _platformRepo.GetPlatformById(id);
+            if (platformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+            return NotFound();
+
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _platformRepo.CreatePlatform(platformModel);
+            _platformRepo.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
+        }
+
+
+
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build-env
+
+WORKDIR /app
+
+EXPOSE 443
+
+COPY *.csproj ./
+
+RUN dotnet restore
+
+COPY . ./
+
+RUN dotnet publish -c Release -o out
+
+FROM mcr.microsoft.com/dotnet/aspnet:5.0
+
+WORKDIR /app
+
+EXPOSE 80
+
+COPY --from=build-env /app/out .
+
+ENTRYPOINT [ "dotnet", "PlatformServices.dll"]
